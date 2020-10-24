1a5ed42261a928159c313254ce902a15ba396686
yirano <yirano@protonmail.ch>
2020-10-23 22:48:21 -0400

    Style button and input field on focus

--- client/src/Components/Todo/Form.jsx
+++ client/src/Components/Todo/Form.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
 
 const Form = () => {
     return (
         <form className="addTask">
             <input type="text" name="todo" placeholder="Add a Task" />
-            <input type="submit" value="Submit" />
+            <button type="submit">
+                <FontAwesomeIcon icon={faArrowUp} />
+            </button>
         </form>
     )
 }
