f3b26ffac9953290590fd5cd240ad9818889dee4
yirano <yirano@protonmail.ch>
2020-10-24 12:02:15 -0400

    Style dashboard Shortcuts links

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles'
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
 import Typography from '@material-ui/core/Typography'
 import ExpandMore from '@material-ui/icons/ExpandMore'
-import { Link } from "react-router-dom"
 import List from "./Todo/List"
 
 
@@ -13,16 +13,18 @@ const Dashboard = () => {
 
     return (
         <div className="dashboard">
-            <Accordion>
+            <Accordion className="accordion">
                 <AccordionSummary
                     expandIcon={<ExpandMore />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                 >
-                    <Typography>Shortcuts</Typography>
+                    <h5>Shortcuts</h5>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <Link to='/today' component={List} />
+                <AccordionDetails className="dashboardLinks">
+                    <Link to='/today' className="dashboardLink">Today</Link>
+                    <Link to='/today' className="dashboardLink">Next 7 Days</Link>
+                    <Link to='/today' className="dashboardLink">All Tasks</Link>
                 </AccordionDetails>
             </Accordion>
         </div>
