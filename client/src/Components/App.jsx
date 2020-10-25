4601e7db98b293760c2acb25519d34d5ac0814cf
yirano <yirano@protonmail.ch>
2020-10-25 11:32:03 -0400

    Set up GoogleCalendar folder to start integrating it into the app but add it into .gitignore for now

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react"
 import Container from "./Container"
 import Dashboard from "./Dashboard"
 import tasks from '../Assets/tasks.json'
+import GoogleSignIn from "./GoogleSignIn"
 
 function App() {
 	return (
 		<div className="app">
 			<Dashboard />
 			<Container tasks={tasks} />
+			<GoogleSignIn />
 		</div>
 	)
 }
